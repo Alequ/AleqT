e2447791eeb237efc23e58610e749e5103241d9f
Alex Turcu <turcu.alex5@gmail.com>
2017-08-22 12:16:16 +0300

    skilsl section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+const devicon = document.querySelectorAll("i");
 
+function addClass() {
+  this.classList.add('colored');
+}
+function removeClass() {
+  this.classList.remove("colored");
+}
+
+devicon.forEach(icon => icon.addEventListener("mouseenter", addClass))
+devicon.forEach(icon => icon.addEventListener("mouseleave", removeClass))
