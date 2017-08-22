3ff13e5cf16f1cd758eaec13c1ef247ecd12c007
Alex Turcu <turcu.alex5@gmail.com>
2017-08-22 17:30:04 +0300

    ss

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ const devicon = document.querySelectorAll("i");
 
 function addClass() {
   this.classList.add('colored');
+
 }
 function removeClass() {
   this.classList.remove("colored");
+  
 }
 
 devicon.forEach(icon => icon.addEventListener("mouseenter", addClass))
