7283b49ade75675ed83b4e63a319fc5cde4224ec
Alex Turcu <turcu.alex5@gmail.com>
2017-08-23 18:59:16 +0300

    scroll spy

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+//toggle class
+
 const devicon = document.querySelectorAll("i");
 
 function addClass() {
@@ -6,8 +9,32 @@ function addClass() {
 }
 function removeClass() {
   this.classList.remove("colored");
-  
+
 }
 
 devicon.forEach(icon => icon.addEventListener("mouseenter", addClass))
 devicon.forEach(icon => icon.addEventListener("mouseleave", removeClass))
+
+
+
+//Smooth scrooling snippet
+
+
+$(".navbar-nav li a[href^='#']").on('click', function(e) {
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top
+     }, 300, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
