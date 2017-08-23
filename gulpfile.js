3413422983d4b401fa28f831820dbfb8251b6d2c
Alex Turcu <turcu.alex5@gmail.com>
2017-08-23 17:11:05 +0300

    type animation

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,26 @@
-const gulp = require("gulp");
-const browserSync = require("browser-sync").create();
-const sass = require("gulp-sass");
 
-//compile sass
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
 
-gulp.task("sass", function(){
-  return gulp.src(["src/scss/*.scss"])
-  .pipe(sass())
-  .pipe(gulp.dest("src/css"))
-  .pipe(browserSync.stream());
-})
-
-//Watch & server
-
-gulp.task("serve", ["sass"], function(){
-  browserSync.init({
-    server: "./"
-  });
+// Compile Sass & Inject Into Browser
+gulp.task('sass', function() {
+    return gulp.src(['src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
 });
 
-gulp.watch(["src/scss*.scss"], ["sass"]);
-gulp.watch(["./*.html"]).on("change", browserSync.reload);
 
-//default
+// Watch Sass & Serve
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./"
+    });
 
-gulp.task("default", ["serve"]);
+    gulp.watch(['src/scss/*.scss'], ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Default Task
+gulp.task('default', ['serve']);
