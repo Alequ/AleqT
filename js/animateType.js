135a7b867eded23f03ddf7c0dca1b8006b102f19
Alex Turcu <turcu.alex5@gmail.com>
2017-09-01 18:24:24 +0300

    ss

--- js/animateType.js
+++ js/animateType.js
@@ -1,5 +1,5 @@
 const typed = new Typed('#typed', {
-     strings: ["<h1>Hi I'm Alex</h1> <h4>Self Taught Web Developer</h4> "],
+     strings: ["<h1>Hi I'm Alex</h1>  <h4>Self Taught Web Developer</h4> "],
      typeSpeed: 50,
      backSpeed: 0,
      loop: true
