38cbd4e23e36c515828c468ba18fdb568c631667
Alex Turcu <turcu.alex5@gmail.com>
2017-09-22 09:40:31 +0300

    fixed typed.js bug

--- js/animateType.js
+++ js/animateType.js
@@ -1,6 +1,7 @@
-const typed = new Typed('#typed', {
-     strings: ["<h1>Hi I'm Alex</h1>  <h4>Self Taught Web Developer</h4> "],
-     typeSpeed: 50,
-     backSpeed: 0,
-     loop: true
-  });
+var typed2 = new Typed('#typed2', {
+  strings: ["Hi, I'm Alex", "Self Taught Web Developer" ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  fadeOut: true,
+  loop: true
+});
