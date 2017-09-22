c55057e1a2bbb35b50f4a70ce268cebb8a76e492
Alex Turcu <turcu.alex5@gmail.com>
2017-09-22 09:59:49 +0300

    responsive added

--- js/animateType.js
+++ js/animateType.js
@@ -1,6 +1,6 @@
 var typed2 = new Typed('#typed2', {
-  strings: ["Hi, I'm Alex", "Self Taught Web Developer" ],
-  typeSpeed: 50,
+  strings: ["Hi, I'm Alex", "Front End Developer" ],
+  typeSpeed: 70,
   backSpeed: 50,
   fadeOut: true,
   loop: true
